0bc3f2dd0a613e3b972eeeacf587073417475033
Robin Beer <contact@robin-beer.com>
2021-06-19 18:34:41 +0200

    Initial Commit

---
+++ audiogame/audiogameApp.swift
@@ -0,0 +1,17 @@
+//
+//  audiogameApp.swift
+//  audiogame
+//
+//  Created by Robin Beer on 19.06.21.
+//
+
+import SwiftUI
+
+@main
+struct audiogameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
